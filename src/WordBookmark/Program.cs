6f6e59401814e429dae20e9de774bb6ce145e7b4
wk-j <somnuk.wk@gmail.com>
2019-02-07 19:45:12 +0700

    Initial commit

---
+++ src/WordBookmark/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using Aspose.Words;
+using System.Linq;
+
+namespace WordBookmark
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = Path.Combine("resource", "Bookmark2.doc");
+            // var path = Path.Combine("resource", "GitHub.doc");
+            var doc = new Document(path);
+            var marks = doc.Range.Bookmarks;
+
+            foreach (var item in marks)
+            {
+                Console.WriteLine(item.Name);
+                Console.WriteLine(item.Text);
+                Console.WriteLine("--------");
+            }
+        }
+
+    }
+}
