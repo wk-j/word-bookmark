5189a01c355478097adccb5573a998cef86da8f5
wk-j <somnuk.wk@gmail.com>
2019-02-07 21:12:08 +0700

    Added omnisharp.json

--- net/WordBookmark/Program.cs
+++ net/WordBookmark/Program.cs
@@ -3,24 +3,30 @@ using System.IO;
 using Aspose.Words;
 using System.Linq;
 
-namespace WordBookmark
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
+namespace WordBookmark {
+    class Callback : IWarningCallback {
+        public void Warning(WarningInfo info) {
+            Console.WriteLine(info.Description);
+        }
+    }
+
+    class Program {
+        static void Main(string[] args) {
             var path = Path.Combine("resource", "Bookmark.doc");
             // var path = Path.Combine("resource", "GitHub.doc");
             var doc = new Document(path);
-            var marks = doc.Range.Bookmarks;
+            // var marks = doc.Range.Bookmarks;
 
-            foreach (var item in marks)
-            {
+            doc.RemoveMacros();
+            doc.WarningCallback = new Callback();
+
+            var marks = doc.Sections.SelectMany(x => x.Range.Bookmarks);
+
+            foreach (var item in marks) {
                 Console.WriteLine(item.Name);
                 Console.WriteLine(item.Text);
                 Console.WriteLine("--------");
             }
         }
-
     }
 }
